e695c8dd682e626b4578cc5657996e8239b2cd27
Utsav Soni <utsavwp103@gmail.com>
2025-10-17 10:02:38 +0530

    Implement cart functionality

--- components/CartButton.tsx
+++ components/CartButton.tsx
@@ -1,11 +1,15 @@
 import { images } from '@/constants';
+import { useCartStore } from '@/store/cart.store';
+import { router } from 'expo-router';
 import React from 'react';
 import { Image, Text, TouchableOpacity, View } from 'react-native';
 
+
 const CartButton = () => {
-  const totalItems = 10; // Example item count
+  const { getTotalItems } = useCartStore();
+    const totalItems = getTotalItems();
   return (
-    <TouchableOpacity className="cart-btn" onPress={()=>{}}>
+    <TouchableOpacity className="cart-btn" onPress={()=>router.push('/cart')}>
         <Image source={images.bag} className="size-6" resizeMode="contain" />
            
            { totalItems > 0 && (

4c7810c2c6e748ebdc9627b368f0a53c0fd21224
Utsav Soni <utsavwp103@gmail.com>
2025-10-17 10:02:38 +0530

    Implement cart functionality

--- components/MenuCard.tsx
+++ components/MenuCard.tsx
@@ -1,10 +1,13 @@
+import { useCartStore } from "@/store/cart.store";
 import { MenuItem } from "@/type";
 import { Image, Platform, Text, TouchableOpacity } from 'react-native';
+
 
 
 const MenuCard = ({ item: { $id, image_url, name, price }}: { item: MenuItem}) => {
     // Use the image_url directly since it's already a complete Appwrite Storage URL
     const imageUrl = image_url;
+    const {addItem} = useCartStore();
     
     console.log('Image URL:', imageUrl);
 
@@ -19,7 +22,7 @@ const MenuCard = ({ item: { $id, image_url, name, price }}: { item: MenuItem}) =
             />
             <Text className="text-center base-bold text-dark-100 mb-2" numberOfLines={1}>{name}</Text>
             <Text className="body-regular text-gray-200 mb-4">From ${price}</Text>
-            <TouchableOpacity onPress={()=>{}}>
+            <TouchableOpacity onPress={()=>addItem({ id: $id, name, price, image_url: imageUrl, customizations: []})}>
                 <Text className="paragraph-bold text-primary">Add to Cart +</Text>
             </TouchableOpacity>
         </TouchableOpacity>
